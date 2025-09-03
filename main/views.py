caae7075fc246a532457ce3f94bcba7c4b5c2ea8
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2025-09-03 13:50:34 +0700

    add show_main function in the views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406362860',
+        'name': 'Muhammad Lanang',
+        'class': 'PBP A'
+    }
+
+    return render(request, "main.html", context)
+
+
