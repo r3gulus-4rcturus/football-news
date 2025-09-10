f14f92a104b92d00771f65e4420899e4b4344239
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2025-09-10 14:05:24 +0700

    tutorial 2 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,6 +34,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-lanang-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://muhammad-lanang-footballnews.pbp.cs.ui.ac.id"
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -61,7 +65,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
